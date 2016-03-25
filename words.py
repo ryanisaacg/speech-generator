729c494878c6d5f68d3e42d45f30dfd8e1c5408f
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-25 13:03:37 -0400

    Switched to splitting on any whitespace instead of just spaces

--- words.py
+++ words.py
@@ -6,7 +6,7 @@ Creates a list of all the words found in the text, in order, in lowercase
 Splits at any space character
 """
 def get_word_list(text):
-    all_words = text.split(" ");
+    all_words = text.split()
     #Matches only non-alphabetic characters
     regex = re.compile('[^a-zA-Z]')
     for i in range(0, len(all_words)):
