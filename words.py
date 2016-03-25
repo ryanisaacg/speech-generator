3863d146c09d5ac04f36cbca10c757e8e254965a
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-25 12:13:02 -0400

    Documented get_word_list function

--- words.py
+++ words.py
@@ -1,7 +1,14 @@
 import re
+"""
+get_word_list(text : String) -> [String]
+Creates a list of all the words found in the text, in order, in lowercase
+Splits at any space character
+"""
 def get_word_list(text):
     all_words = text.split(" ");
+    #Matches only non-alphabetic characters
     regex = re.compile('[^a-zA-Z]')
     for i in range(0, len(all_words)):
+        #Get the word in lower case, with all non-alphabetic characters removed
         all_words[i] = regex.sub('', all_words[i]).lower()
     return all_words
