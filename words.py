205f1414f4462012bf86f18feb2048fa0dd5f575
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-25 12:25:56 -0400

    Created a function that maps a word to a list of maps of how often that word is used

--- words.py
+++ words.py
@@ -12,3 +12,26 @@ def get_word_list(text):
         #Get the word in lower case, with all non-alphabetic characters removed
         all_words[i] = regex.sub('', all_words[i]).lower()
     return all_words
+LOOK_AHEAD_AMT = 10
+"""
+get_word_map(word_list : [String]) -> {String : LOOK_AHEAD_AMT[{String : Int}]}
+Creates a map of words that are often found in the next ten words after a specific word
+"""
+def get_word_map(word_list):
+    word_map = {} #{String :{String : LOOK_AHEAD_AMT[{String : Int}]}
+    #Increment a key/value pair in the word map
+    def add_to_entry(key_word, index, value_word):
+        if value_word in word_map[key_word][index]:
+            word_map[key_word][index][value_word] += 1
+        else:
+            word_map[key_word][index][value_word] = 1
+    for i in range(0, len(word_list)):
+        word = word_list[i]
+        if not word in word_map:
+            word_map[word] = [{} for x in range(0, LOOK_AHEAD_AMT)]
+        j = i + 1
+        while j < i + 10 and j < len(word_list):
+            add_to_entry(word, j - i - 1, word_list[j])
+            j += 1
+    return word_map
+print(get_word_map(get_word_list("Hello, hello, hello- oh no!")));
